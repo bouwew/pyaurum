f17f7c53a0db8ccce37d4afc5c48a9673f927837
Bouwe Westerdijk <11290930+bouwew@users.noreply.github.com>
2020-04-07 16:33:19 +0200

    Update config_flow.py

--- custom_components/pyaurum/config_flow.py
+++ custom_components/pyaurum/config_flow.py
@@ -1,69 +1,74 @@
-"""Adds config flow for Brother Printer."""
-import ipaddress
-import re
+"""Config flow for Plugwise Anna integration."""
+import logging
+from typing import Any, Dict
 
-from .py_aurum import Aurum
 import voluptuous as vol
 
-from homeassistant import config_entries, exceptions
-from homeassistant.const import CONF_HOST
+from homeassistant import config_entries, core, exceptions
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from .py_aurum import Aurum
 
-from .const import DOMAIN
+from .const import DOMAIN  # pylint:disable=unused-import
 
-DATA_SCHEMA = vol.Schema(
-    {
-        vol.Required(CONF_HOST, default=""): str,
-    }
-)
+_LOGGER = logging.getLogger(__name__)
 
 
-def host_valid(host):
-    """Return True if hostname or IP address is valid."""
-    try:
-        if ipaddress.ip_address(host).version == (4 or 6):
-            return True
-    except ValueError:
-        disallowed = re.compile(r"[^a-zA-Z\d\-]")
-        return all(x and not disallowed.search(x) for x in host.split("."))
+def _get_config_schema(input_dict: Dict[str, Any] = None) -> vol.Schema:
+    """
+    Return schema defaults for init step based on user input/config dict.
+
+    Retain info already provided for future form views by setting them as defaults in schema.
+    """
+    if input_dict is None:
+        input_dict = {}
+
+    return vol.Schema({vol.Required("host"): str})
 
 
-class AurumConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for Aurum meetstekker."""
+async def validate_input(hass: core.HomeAssistant, data):
+    """
+    Validate the user input allows us to connect.
+
+    Data has the keys from DATA_SCHEMA with values provided by the user.
+    """
+    websession = async_get_clientsession(hass, verify_ssl=False)
+    api = Aurum(
+        host=data["host"], timeout=10, websession=websession
+    )
+
+    if not await api.async_update():
+        raise CannotConnect
+
+    return True
+
+
+class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for Aurum Meetstekker."""
 
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
 
-    def __init__(self):
-        """Initialize."""
-        self.aurum = None
-        self.host = None
-
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
         errors = {}
-
+        self._user_schema = _get_config_schema(user_input)
         if user_input is not None:
+
             try:
-                if not host_valid(user_input[CONF_HOST]):
-                    raise InvalidHost()
+                info = await validate_input(self.hass, user_input)
 
-                api = Aurum(user_input[CONF_HOST])
-                await api.async_update()
-
-                #await self.async_set_unique_id(brother.serial.lower())
-                #self._abort_if_unique_id_configured()
-
-                title = "Aurum Meetstekker"
-                return self.async_create_entry(title=title, data=user_input)
-            except InvalidHost:
-                errors[CONF_HOST] = "wrong_host"
-            except ConnectionError:
-                errors["base"] = "connection_error"
+                return self.async_create_entry(title=info["title"], data=user_input)
+            except CannotConnect:
+                errors["base"] = "cannot_connect"
+            except Exception:  # pylint: disable=broad-except
+                _LOGGER.exception("Unexpected exception")
+                errors["base"] = "unknown"
 
         return self.async_show_form(
-            step_id="user", data_schema=DATA_SCHEMA, errors=errors
+            step_id="user", data_schema=self._user_schema, errors=errors
         )
 
 
-class InvalidHost(exceptions.HomeAssistantError):
-    """Error to indicate that hostname/IP address is invalid."""
+class CannotConnect(exceptions.HomeAssistantError):
+    """Error to indicate we cannot connect."""
+    
